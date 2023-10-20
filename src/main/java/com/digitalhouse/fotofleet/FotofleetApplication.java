4b087804a70ce197264dc9481fb5052f8d8f873f
Gafhito <gonzalo.ferrari@outlook.es>
2023-10-19 23:14:35 -0300

    Creación del proyecto

---
+++ src/main/java/com/digitalhouse/fotofleet/FotofleetApplication.java
@@ -0,0 +1,11 @@
+package com.digitalhouse.fotofleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotofleetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FotofleetApplication.class, args);
+    }
+}
---
+++ src/test/java/com/digitalhouse/fotofleet/FotofleetApplicationTests.java
@@ -0,0 +1,11 @@
+package com.digitalhouse.fotofleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotofleetApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
