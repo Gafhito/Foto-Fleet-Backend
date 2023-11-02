10d3d039dfa0d5c62209e09ec6cb3738b50d7624
Gafhito <gonzalo.ferrari@outlook.es>
2023-11-02 15:16:13 -0300

    Corregido uso de log4j en GlobalExceptions

--- src/main/java/com/digitalhouse/fotofleet/exceptions/GlobalExceptions.java
+++ src/main/java/com/digitalhouse/fotofleet/exceptions/GlobalExceptions.java
@@ -1,16 +1,15 @@
 package com.digitalhouse.fotofleet.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.apache.log4j.Logger;
-
 
 @ControllerAdvice
 public class GlobalExceptions {
-    private static final Logger logger = Logger.getLogger(GlobalExceptions.class);
-
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptions.class);
 
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<String> procesarErrorNotFound(ResourceNotFoundException ex){
