ae911402bec90c465765338240089b5442e498e3
Gafhito <gonzalo.ferrari@outlook.es>
2023-11-09 19:13:02 -0300

    Agregando reenvío de email

--- src/main/java/com/digitalhouse/fotofleet/services/AuthService.java
+++ src/main/java/com/digitalhouse/fotofleet/services/AuthService.java
@@ -25,26 +25,7 @@ public class AuthService {
     private final JwtGenerator jwtGenerator;
     private final UserService userService;
     private final RolService rolService;
-
-     //Inyectando EmailSenderService
-     @Autowired
-     private EmailSenderService EmailSenderService;
-
-    public void enviarCorreo(String toEmail, String firstName, String lastName) {
-        // Construye el asunto y el cuerpo del mail llamando al usuario
-        String subject = "¡Bienvenido a nuestra aplicación!";
-
-        // Construye el cuerpo del correo con un enlace HTML
-        String body = "Hola " + firstName + " " + lastName + ",<br><br>"
-                + "Te has registrado exitosamente en nuestra aplicación. ¡Bienvenido!<br><br>"
-                + "Gracias por unirte a nosotros. Para acceder a la aplicación, haz clic en el siguiente enlace:<br>"
-                + "<a href=\"http://1023c07-grupo3.s3-website-us-east-1.amazonaws.com/\">Acceder a la aplicación</a>";
-
-
-        // Llama al metodo sendMail de EmailSenderService
-        EmailSenderService.sendEmail(toEmail, subject, body);
-    }
-
+    private final EmailSenderService emailSenderService;
 
     public User register(String roleName, RegisterDto registerDto) {
         User user = new User(
@@ -60,7 +41,7 @@ public class AuthService {
         user.setRoles(Collections.singletonList(rol));
 
         // Después de registrar al usuario, llama al método para enviar el correo personalizado
-        enviarCorreo(registerDto.email(), registerDto.firstName(), registerDto.lastName());
+        emailSenderService.enviarCorreo(registerDto.email(), registerDto.firstName(), registerDto.lastName());
 
 
         return userService.createUser(user);
