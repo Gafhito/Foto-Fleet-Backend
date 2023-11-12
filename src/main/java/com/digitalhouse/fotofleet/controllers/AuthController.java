a20633e568cc215939582b57dabb8788b4d2cf67
Gafhito <gonzalo.ferrari@outlook.es>
2023-11-12 14:03:11 -0300

    Documentación de AuthController y CategoryController

--- src/main/java/com/digitalhouse/fotofleet/controllers/AuthController.java
+++ src/main/java/com/digitalhouse/fotofleet/controllers/AuthController.java
@@ -2,48 +2,65 @@ package com.digitalhouse.fotofleet.controllers;
 
 import com.digitalhouse.fotofleet.dtos.LoginDto;
 import com.digitalhouse.fotofleet.dtos.RegisterDto;
+import com.digitalhouse.fotofleet.dtos.ResponseLoginDto;
 import com.digitalhouse.fotofleet.dtos.UpdateRolDto;
 import com.digitalhouse.fotofleet.exceptions.BadRequestException;
 import com.digitalhouse.fotofleet.exceptions.ResourceNotFoundException;
+import com.digitalhouse.fotofleet.exceptions.ResponseException;
 import com.digitalhouse.fotofleet.models.User;
 import com.digitalhouse.fotofleet.services.AuthService;
 import com.digitalhouse.fotofleet.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth", description = "Permite el registro de usuarios y la autenticación de los mismos")
 public class AuthController {
     private final AuthService authService;
     private final UserService userService;
 
-    @PostMapping("/register/admin")
-    public ResponseEntity<?> registerAdmin(@RequestBody RegisterDto registerDto) throws BadRequestException {
-        return new ResponseEntity<>(authService.register("Admin", registerDto), HttpStatus.CREATED);
-    }
-
+    @Operation(summary = "Registrar Moderador", description = "Permite el registro de usuarios con un nivel de Moderador", responses = {
+            @ApiResponse(responseCode = "209", description = "Usuario creado con éxito", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Error al registrar usuario", content = @Content(schema = @Schema(implementation = ResponseException.class)))
+    })
     @PostMapping("/register/moderator")
     public ResponseEntity<?> registerModerator(@RequestBody RegisterDto registerDto) throws BadRequestException {
         return new ResponseEntity<>(authService.register("Moderator", registerDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Registrar Usuario", description = "Permite el registro de usuarios con un nivel de privilegios mínimo", responses = {
+            @ApiResponse(responseCode = "209", description = "Usuario creado con éxito", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Error al registrar usuario", content = @Content(schema = @Schema(implementation = ResponseException.class)))
+    })
     @PostMapping("/register/user")
     public ResponseEntity<?> registerUser(@RequestBody RegisterDto registerDto) throws BadRequestException {
         return new ResponseEntity<>(authService.register("User", registerDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Login de usuarios", description = "Permite el login de usuarios al sistema", responses = {
+            @ApiResponse(responseCode = "200", description = "Login exitoso", content = @Content(schema = @Schema(implementation = ResponseLoginDto.class))),
+            @ApiResponse(responseCode = "403", description = "Error al iniciar sesión")
+    })
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
         return new ResponseEntity<>(authService.login(loginDto), HttpStatus.OK);
     }
 
+    @Operation(summary = "Actualizar rol de usuarios", description = "Solo para usuarios administradores, permite actualizar el rol de usuarios moderadores o de bajo nivel", responses = {
+            @ApiResponse(responseCode = "200", description = "Actualización de rol exitosa", content = @Content(schema = @Schema(implementation = UpdateRolDto.class))),
+            @ApiResponse(responseCode = "400", description = "No se puede asignar rol Admin a un usuario", content = @Content(schema = @Schema(implementation = ResponseException.class))),
+            @ApiResponse(responseCode = "403", description = "No tiene permisos para realizar dicha acción"),
+            @ApiResponse(responseCode = "404", description = "No existe el rol que ha intentado asignar", content = @Content(schema = @Schema(implementation = ResponseException.class)))
+    })
     @PostMapping("/update")
     public ResponseEntity<?> updateRol(@RequestBody UpdateRolDto updateRolDto) throws ResourceNotFoundException, BadRequestException {
         return new ResponseEntity<>(userService.updateRol(updateRolDto), HttpStatus.OK);
--- src/main/java/com/digitalhouse/fotofleet/controllers/CategoryController.java
+++ src/main/java/com/digitalhouse/fotofleet/controllers/CategoryController.java
@@ -3,7 +3,14 @@ package com.digitalhouse.fotofleet.controllers;
 import com.digitalhouse.fotofleet.dtos.CategoryDto;
 import com.digitalhouse.fotofleet.exceptions.BadRequestException;
 import com.digitalhouse.fotofleet.exceptions.ResourceNotFoundException;
+import com.digitalhouse.fotofleet.exceptions.ResponseException;
+import com.digitalhouse.fotofleet.models.Category;
 import com.digitalhouse.fotofleet.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,36 +20,63 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
+@Tag(name = "Category", description = "Permite la creación, el listado, la actualización y borrado de categorías de productos")
 public class CategoryController {
-
     private final CategoryService categoryService;
 
+    @Operation(summary = "Creación de categoría", description = "Permite la creación de una nueva categoría", responses = {
+            @ApiResponse(responseCode = "209", description = "Categoría creada", content = @Content(schema = @Schema(implementation = Category.class))),
+            @ApiResponse(responseCode = "403", description = "No tiene permisos para realizar dicha acción")
+    })
     @PostMapping
-    public ResponseEntity<?> createCategory(@RequestBody CategoryDto categoryDto){
+    public ResponseEntity<?> createCategory(@RequestBody CategoryDto categoryDto) {
         return new ResponseEntity<>(categoryService.createCategory(categoryDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Asignación de imagen a una categoría", description = "Permite cargar una imagen a una categoría que no la tenga", responses = {
+            @ApiResponse(responseCode = "209", description = "Imagen almacenada exitosamente", content = @Content(schema = @Schema(implementation = Category.class))),
+            @ApiResponse(responseCode = "400", description = "Error al procesar la imagen", content = @Content(schema = @Schema(implementation = ResponseException.class))),
+            @ApiResponse(responseCode = "403", description = "No tiene permisos para realizar dicha acción"),
+            @ApiResponse(responseCode = "404", description = "No existe la categoría del ID proporcionado", content = @Content(schema = @Schema(implementation = ResponseException.class)))
+    })
     @PostMapping("/image")
     public ResponseEntity<?> uploadImage(@RequestParam Integer categoryId, @RequestParam MultipartFile file) throws BadRequestException, ResourceNotFoundException {
         return new ResponseEntity<>(categoryService.uploadImage(categoryId, file), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Obtener categoría por ID", description = "Permite obtener una categoría a través del ID proporcionado en la ruta de la URL", responses = {
+            @ApiResponse(responseCode = "200", description = "Categoría obtenida exitosamente", content = @Content(schema = @Schema(implementation = Category.class))),
+            @ApiResponse(responseCode = "404", description = "No existe la categoría del ID proporcionado", content = @Content(schema = @Schema(implementation = ResponseException.class)))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<?> getCategoryById(@PathVariable Integer id) throws ResourceNotFoundException {
         return new ResponseEntity<>(categoryService.getCategoryById(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Obtener listado de categorías", description = "Obtiene el listado de todas las categorías existentes al momento", responses = {
+            @ApiResponse(responseCode = "200", description = "Listado obtenido exitosamente", content = @Content(schema = @Schema(implementation = Category.class)))
+    })
     @GetMapping
     public ResponseEntity<?> listCategories() {
         return new ResponseEntity<>(categoryService.listCategories(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Elimina una categoría", description = "Elimina categoría a través del ID proporcionado en la URL de la petición", responses = {
+            @ApiResponse(responseCode = "204", description = "Categoría eliminada exitosamente"),
+            @ApiResponse(responseCode = "403", description = "No tiene permisos para realizar dicha acción"),
+            @ApiResponse(responseCode = "404", description = "No existe la categoría del ID proporcionado", content = @Content(schema = @Schema(implementation = ResponseException.class)))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCategory(@PathVariable Integer id) throws ResourceNotFoundException{
         categoryService.deleteCategory(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Actualizar categoría", description = "Actualiza una categoría a través del ID proporcionado en la URL de la petición", responses = {
+            @ApiResponse(responseCode = "200", description = "Categoría actualizada exitosamente", content = @Content(schema = @Schema(implementation = Category.class))),
+            @ApiResponse(responseCode = "403", description = "No tiene permisos para realizar dicha acción"),
+            @ApiResponse(responseCode = "404", description = "No existe la categoría del ID proporcionado", content = @Content(schema = @Schema(implementation = ResponseException.class)))
+    })
     @PutMapping("/{id}")
     public ResponseEntity<?> updateCategory(@PathVariable Integer id, @RequestBody CategoryDto categoryDto) throws ResourceNotFoundException {
         return new ResponseEntity<>(categoryService.updateCategory(id, categoryDto),HttpStatus.OK);
